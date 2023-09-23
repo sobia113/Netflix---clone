a7e0834530946d7b2c488131d76cade30dd365a7
sobia113 <114018087+sobia113@users.noreply.github.com>
2023-09-23 16:29:52 +0500

    Add files via upload

---
+++ Netflixclone/app.js
@@ -0,0 +1,5 @@
+// For Chrome, Safari, and Opera
+document.documentElement.style.setProperty('--scrollbar-width', `${window.innerWidth - document.documentElement.clientWidth}px`);
+
+// For Firefox
+document.documentElement.style.setProperty('--scrollbar-height', `${window.innerHeight - document.documentElement.clientHeight}px`);
